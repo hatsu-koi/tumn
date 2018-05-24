a9cb73c6dfe3f2dbc89144d88a521c50508ef589
Khinenw <deu07115@gmail.com>
2018-05-24 23:28:56 +0900

    Oops

--- app/layouts/SectionFilters.vue
+++ app/layouts/SectionFilters.vue
@@ -8,21 +8,21 @@
 			:title="$t('settings.need_processor')"
 			:desc="$t('settings.need_processor_cover')">
 		</warning-pane>
+
+		<setting-item>
+			<span slot="title">
+				{{$t('settings.user_dict')}}
+			</span>
+
+			<span slot="description">
+				{{$t('settings.user_dict_desc')}}
+			</span>
+
+			<button class="Button" v-ripple="'rgba(255, 255, 255, .2)'" slot="option">
+				{{$t('settings.user_dict_edit')}}
+			</button>
+		</setting-item>
 	</config-list>
-
-	<setting-item>
-		<span slot="title">
-			{{$t('settings.user_dict')}}
-		</span>
-
-		<span slot="description">
-			{{$t('settings.user_dict_desc')}}
-		</span>
-
-		<button class="Button" v-ripple="'rgba(255, 255, 255, .2)'" slot="option">
-			{{$t('settings.user_dict_edit')}}
-		</button>
-	</setting-item>
 </template>
 
 <script>
